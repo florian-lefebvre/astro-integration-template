0f9d12c024b2a9905e16bcb8f77527c376db4f6e
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-01-20 17:30:56 +0100

    Update astro.config.mjs

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,13 +1,11 @@
 import tailwind from "@astrojs/tailwind";
-import astroTailwindConfigViewer from "astro-tailwind-config-viewer";
+import packageName from "package-name";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		tailwind(),
-		astroTailwindConfigViewer({
-			viewer: true,
-		}),
+		packageName(),
 	],
 });
