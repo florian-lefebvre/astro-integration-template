4841eb5afc339af9d170fa188391d05f7bdcd40f
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-22 18:08:07 +0100

    feat: hmr experiments

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -3,6 +3,7 @@ import { defineIntegration } from "astro-integration-kit";
 export const integration = defineIntegration({
 	name: "package-name",
 	setup() {
+		console.log("abc")
 		return {};
 	},
 });
