188c779b7d93a2cb285ed0bfa18748d2b28fb632
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-25 13:48:49 +0100

    chore: remove hmr experiments

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -3,7 +3,6 @@ import { defineIntegration } from "astro-integration-kit";
 export const integration = defineIntegration({
 	name: "package-name",
 	setup() {
-		console.log("abc")
 		return {};
 	},
 });
--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,39 +1,8 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import packageName from "package-name";
-import {
-	defineIntegration,
-	createResolver,
-} from "../package/node_modules/astro-integration-kit/dist/core/index.js";
-import { watchIntegration } from "../package/node_modules/astro-integration-kit/dist/utilities/index.js";
-import { z } from "astro/zod";
-import { fileURLToPath } from "node:url";
-
-const hmrIntegration = defineIntegration({
-	name: "hmr",
-	optionsSchema: z.object({
-		path: z.string(),
-	}),
-	setup({ options }) {
-		return {
-			"astro:config:setup": (params) => {
-				const { config } = params;
-
-				const { resolve } = createResolver(fileURLToPath(config.root));
-				const dir = resolve(options.path);
-				console.log({ dir });
-
-				watchIntegration({ ...params, dir });
-			},
-		};
-	},
-});
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		packageName(),
-		hmrIntegration({ path: "../package/dist" }),
-	],
+	integrations: [tailwind(), packageName()],
 });
