67cfa9f78d5118c893248f1fa75c5a3adc5e111e
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-15 13:25:59 +0200

    Update integration.ts

--- package/src/integration.ts
+++ package/src/integration.ts
@@ -3,6 +3,8 @@ import { defineIntegration } from "astro-integration-kit";
 export const integration = defineIntegration({
 	name: "package-name",
 	setup() {
-		return {};
+		return {
+			hooks: {},
+		};
 	},
 });
