5e406855417181e06d71e71922e9234d5846dc03
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-10-20 16:56:30 +0200

    feat: docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,36 +4,35 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-integration-template.netlify.app",
-	integrations: [
-		starlight({
-			title: "Astro Integration Template",
-			logo: {
-				src: "./src/assets/houston.webp",
-			},
-			social: [
-				{
-					icon: "github",
-					label: "GitHub",
-					href: "https://github.com/florian-lefebvre/astro-integration-template",
-				},
-			],
-			sidebar: [
-				{
-					label: "Get Started",
-					items: [
-						{ label: "Prerequisites", link: "/get-started/prerequisites/" },
-						{ label: "Pick a template", link: "/get-started/pick-a-template/" },
-					],
-				},
-				{
-					label: "Templates",
-					items: [
-						{ label: "Manual (AIK)", link: "/templates/manual-kit/" },
-						{ label: "GitHub (AIK)", link: "/templates/github-kit/" },
-					],
-				},
-			],
-		}),
-	],
+  site: "https://astro-integration-template.netlify.app",
+  integrations: [
+    starlight({
+      title: "Astro Integration Template",
+      logo: {
+        src: "./src/assets/houston.webp",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/florian-lefebvre/astro-integration-template",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Get Started",
+          items: [
+            { label: "Prerequisites", link: "/get-started/prerequisites/" },
+            { label: "Pick a template", link: "/get-started/pick-a-template/" },
+          ],
+        },
+        {
+          label: "Templates",
+          items: [
+            { label: "GitHub (AIK)", link: "/templates/github-kit/" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
